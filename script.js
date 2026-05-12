6aaa97e33365c66320c07a18aed7ba5ada8c2478
mateusdefreitasienh <mateusdefreitas@ienh.com.br>
2026-05-11 21:46:23 -0300

    array filter e reduce

--- script.js
+++ script.js
@@ -43,4 +43,15 @@ console.log(respostasFormatadas);
 const breakingBadCharacters = ["Walter White", "Jesse Pinkman", "Skyler White", "Hank Schrader", "Saul Goodman", "Gustavo Fring", "Mike Ehrmantraut", "Tuco Salamanca", "Jane Margolis", "Hector Salamanca"];
 
 const iniciais = breakingBadCharacters.map(nome => nome[0])
-console.log(iniciais)
+console.log(iniciais);
+
+// Selecionar menores que 20 -> Uso do filter
+const idades = [19, 22, 15, 30, 18, 25, 17, 20, 21, 16];
+
+const menoresVinte = idades.filter(idade => idade <= 20 ? true : false);
+console.log(menoresVinte);
+
+// Somatório das notas -> Uso do reduce
+notas = [3, 7, 9, 10, 3];
+const somaNotas = notas.reduce((acumulador, nota) => acumulador + nota, 0); // acumulador + item do array => regra de acululação (aculumador + nota), valor incial acumulador
+console.log(somaNotas);
