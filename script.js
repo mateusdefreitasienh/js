03a4d7aaad49e321660e6ef0fbfbf5dda70d6fc1
Mateus de Freitas <mateusdefreitas@ienh.com.br>
2026-05-25 21:22:40 -0300

    exibir elementos em tela

--- script.js
+++ script.js
@@ -201,12 +201,34 @@ const pessoas = [{
 {
     nome: "Joao",
     sobrenome: "Carlos",
-    idade: 27,
+    idade: 32,
     mostarIdade() {
         alert(this.nome + " " + this.sobrenome)
     },
 }];
 
 // desestruturar diretamente no map
-const algo = pessoas.map(({nome, sobrenome}) => `${nome} ${sobrenome}`)
-console.log(algo)
+const nomesCompletos = pessoas.map(({nome, sobrenome}) => `${nome} ${sobrenome}`);
+
+
+// Exibir elementos em tela
+
+const ul = document.querySelector('#nomes');
+
+const nomesCompletosLi = nomesCompletos.map(nomeCompleto => {
+    return `<li>${nomeCompleto}</li>`;
+});
+
+// ul.innerHTML = nomesCompletosLi.join('');
+
+const pessoasLi = pessoas.map(({nome, sobrenome, idade}) => {
+    const pessoa = document.createElement('li');
+    pessoa.innerHTML = `${nome} ${sobrenome}`;
+    pessoa.className = 'pessoa';
+    pessoa.onclick = () => {
+        alert(`A idade da pessoa é ${idade}`);
+    };
+    return pessoa;
+});
+
+ul.append(...pessoasLi)
